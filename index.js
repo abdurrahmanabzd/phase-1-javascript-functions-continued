2a77eeaa6466e3c147e1e4b2585bc9b9daedfc0d
Abdurrahman Abuzaid <abdurrahman.abzd@gmail.com>
2023-04-19 20:41:39 -0400

    solve assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(act='roller-skate'){
+    return `This Saturday, I want to ${act}!`
+}
+
+function mondayWork(work='go to the office'){
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(par="*"){
+    return function inner(parm="special"){
+        return `You are ${par}${parm}${par}!`
+    }
+}
